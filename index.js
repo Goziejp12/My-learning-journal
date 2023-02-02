3c3e556c6475826c6b2701f593cbfa5d70d5c4cb
Chigozie Johnpaul Maduka <chigoziemaduka401@gmail.com>
2023-02-02 21:24:27 +0100

    done

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const viewMore = document.getElementById('view-more')
 const intro = document.getElementById('intro')
+const viewMore = document.getElementById('view-more')
+const anchorBtn = document.getElementById('anchor-btn')
+const aboutMe = document.querySelector('.about-me')
 
 document.addEventListener('click', function(e) {
     if (e.target.id == 'anchor-btn') {
-        viewMore.style.display = 'block'
         intro.style.display = 'none'
+        viewMore.style.display = 'block'
+        anchorBtn.style.display = 'none'
+    }
+    else if (e.target.id == 'about-me') {
+        intro.style.display = 'none'
+        aboutMe.style.display = 'block'
+        anchorBtn.style.display = 'none'
     }
 })
